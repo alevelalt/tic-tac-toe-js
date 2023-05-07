559289626b6717deccda6169e8a28fa8ebde532d
alevelalt <alevelalt@gmail.com>
2023-05-08 01:03:52 +0600

    added all basic event listeners

--- SCRIPT/app.js
+++ SCRIPT/app.js
@@ -4,11 +4,26 @@ const App = {
     menuItems: document.querySelector("[data-id='menu-items']"),
     resetBtn: document.querySelector("[data-id='reset-btn']"),
     newRoundBtn: document.querySelector("[data-id='new-round-btn']"),
+    gameSquares: document.querySelectorAll("[data-id='main-game-squares']"),
   },
 
   init() {
     App.$.actionMenu.addEventListener("click", () => {
       App.$.menuItems.classList.toggle("hidden");
+    });
+
+    App.$.resetBtn.addEventListener("click", () => {
+      console.log("Reset Game.");
+    });
+
+    App.$.newRoundBtn.addEventListener("click", () => {
+      console.log("Go to new round.");
+    });
+
+    App.$.gameSquares.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        console.log(`Square with ID: ${event.target.id} was clicked`);
+      });
     });
   },
 };
