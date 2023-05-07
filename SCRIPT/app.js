80a18027fb2977562f6001c0b306501da5958354
alevelalt <alevelalt@gmail.com>
2023-05-07 23:58:49 +0600

    code improvement

--- SCRIPT/app.js
+++ SCRIPT/app.js
@@ -3,8 +3,12 @@ const App = {
     actionMenu: document.querySelector(".menu"),
     menuItems: document.querySelector(".items"),
   },
+
+  init() {
+    App.$.actionMenu.addEventListener("click", () => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+  },
 };
 
-App.$.actionMenu.addEventListener("click", () => {
-  App.$.menuItems.classList.toggle("hidden");
-});
+window.addEventListener("load", App.init);
